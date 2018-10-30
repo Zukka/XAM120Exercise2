ed9b12d498ef22ebfda03123c82849f68d1c1a77
Alessandro <a.bellotti@patente.it>
2018-10-30 10:31:16 +0100

    Aggiungere i file di progetto.

---
+++ Phoneword/Phoneword/MainPage.cs
@@ -0,0 +1,61 @@
+﻿using Xamarin.Forms;
+
+namespace Phoneword
+{
+    public class MainPage : ContentPage
+    {
+        Entry entry;
+        Button btnCall;
+
+        public MainPage()
+        {
+            entry = new Entry
+            {
+                Text = "1-855-XAMARIN"
+            };
+
+            Button btnTranslate = new Button
+            {
+                Text = "Translate"
+            };
+            btnTranslate.Clicked += OnTranslate;
+
+            btnCall = new Button
+            {
+                Text = "Call",
+                IsEnabled = false
+            };
+
+            Content = new StackLayout
+            {
+                VerticalOptions = LayoutOptions.Start,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                Children = {
+                    new Label {
+                        HorizontalTextAlignment = TextAlignment.Center,
+                        Text = "Enter a Phoneword:"
+                    },
+                    entry,
+                    btnTranslate,
+                    btnCall
+                }
+            };
+        }
+
+        private void OnTranslate(object sender, System.EventArgs e)
+        {
+            string translatedNumber = Core.PhonewordTranslator.ToNumber(entry.Text);
+
+            if(string.IsNullOrEmpty(translatedNumber))
+            {
+                btnCall.Text = "Call";
+                btnCall.IsEnabled = false;
+            }
+            else
+            {
+                btnCall.Text = "Call" + translatedNumber;
+                btnCall.IsEnabled = true;
+            }
+        }
+    }
+}
